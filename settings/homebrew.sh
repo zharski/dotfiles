98aa173c3c235209e4af088a279efea1b39663a3
Ihar Zharski <iharzharski@Ihars-MacBook-Pro.local>
2020-04-01 16:56:18 +0200

    added homebrew script

---
+++ settings/homebrew.sh
@@ -0,0 +1,9 @@
+# Install Brew Packages
+brew install python
+brew install tree
+
+# Install MacOS Applications
+brew cask install google-chrome
+brew cask install skype
+brew cask install vlc
+brew cask install spotify
