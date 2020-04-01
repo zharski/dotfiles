8f81510ba81243b40e46700cc5ecc3350600b8ab
Ihar Zharski <iharzharski@Ihars-MacBook-Pro.local>
2020-04-01 17:09:55 +0200

    update brew packages

--- settings/homebrew.sh
+++ settings/homebrew.sh
@@ -3,7 +3,16 @@ brew install python
 brew install tree
 
 # Install MacOS Applications
+brew cask install alfred
+brew cask install caffeine
+brew cask install dropbox
 brew cask install google-chrome
+brew cask install iterm2
 brew cask install skype
-brew cask install vlc
+brew cask install slack
+brew cask install sourcetree
 brew cask install spotify
+brew cask install vlc
+
+brew cleanup
+brew cask cleanup
