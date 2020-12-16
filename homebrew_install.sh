0bf11d9dc155e08116056bf3dbb545ea4c05bc7f
Ihar Zharski <ihar.zharski@gmail.com>
2020-12-16 23:39:28 +0100

    small tweaks for iTerm2

--- homebrew_install.sh
+++ homebrew_install.sh
@@ -3,14 +3,18 @@ brew install python
 brew install tree
 
 # Install MacOS Applications
+
+brew cask install iterm2
+brew install zsh zsh-completions
+brew install curl
+brew cask install sourcetree
+
 brew cask install alfred
 brew cask install caffeine
 brew cask install dropbox
 brew cask install google-chrome
-brew cask install iterm2
 brew cask install skype
 brew cask install slack
-brew cask install sourcetree
 brew cask install spotify
 brew cask install vlc
 
